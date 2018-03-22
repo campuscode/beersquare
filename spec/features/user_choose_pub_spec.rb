ebdc1fb36db5f59ea7cefd1ebba6f52fc07c75da
Francisco e Mayumi <franciscofelixs@gmail.com>
2018-03-22 20:13:27 -0300

    [WIP] user choose pub

--- spec/features/user_choose_pub_spec.rb
+++ spec/features/user_choose_pub_spec.rb
@@ -10,17 +10,22 @@ feature 'user choose bar' do
 
     bar = Bar.create(name: 'CampusCode Pub', state: 'SP', city: 'São Paulo',
       neighborhood: 'Jardins', zip_code: '01234-001', phone: '11-55006789',
-      trading_hour: 'segunda à sexta das 17h às 0h, sábado e domingo das 15h às 2h',
-      payment: 'dinheiro, todos os cartões de crédito e débito; não aceita
-      cheques e ticket', rank: '5', services: 'cozinha, valet, música ao vivo')
+      trading_hour: '17:00 - 00:00', payment: 'dinheiro, cartões de crédito e débito',
+      rank: '5.0', services: 'cozinha, valet, música ao vivo', beer: cerveja1 )
 
     visit pub_details_path
 
-    expect(page).to have_css('h3', text: 'CampusCode Pub' )
-    expect(page).to have_css('li', text: 'São Paulo' )
-    expect(page).to have_css('li', text: 'CampusCode Pub' )
-
-
+    expect(page).to have_css('h3', text: 'CampusCode Pub')
+    expect(page).to have_css('li', text: 'Jardins')
+    expect(page).to have_css('li', text: 'São Paulo')
+    expect(page).to have_css('li', text: 'SP')
+    expect(page).to have_css('li', text: '01234-001')
+    expect(page).to have_css('li', text: '11-55006789')
+    expect(page).to have_css('li', text: '17:00 - 00:00')
+    expect(page).to have_css('li', text: 'dinheiro, cartões de crédito e débito')
+    expect(page).to have_css('li', text: '5.0')
+    expect(page).to have_css('li', text: 'cozinha, valet, música ao vivo')
+    expect(page).to have_css('h3', text: "Cervejas: #{bar.beer}" )
 
 
   end
