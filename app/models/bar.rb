174c06b1c32bfa6b0b76e514c5458c2f5b490c2d
Koike <edu.fox@gmail.com>
2018-03-26 20:09:17 -0300

    Add checkin model and associations

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,4 +1,5 @@
 class Bar < ApplicationRecord
   has_many :bar_beers, dependent: :destroy
   has_many :beers, through: :bar_beers
+  has_many :checkins, dependent: :destroy
 end
---
+++ spec/factories/checkins.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :checkin do
+    bar nil
+    beer nil
+  end
+end
