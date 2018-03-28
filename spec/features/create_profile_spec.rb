1ae79c5cbe99c186b79b37da1fe587d5513d301b
Francisco e Mayumi <franciscofelixs@gmail.com>
2018-03-28 18:39:45 -0300

    Changes according to suggestions in PR

--- spec/features/create_profile_spec.rb
+++ spec/features/create_profile_spec.rb
@@ -20,19 +20,21 @@ feature 'create profile' do
 
   scenario 'When the user already has a profile' do
     user = create(:user)
-    create(:profile, user: user)
+    profile = Profile.last
 
     login_as(user, scope: :user)
     visit root_path
     click_on 'Meu perfil'
     click_on 'Editar'
     fill_in 'Nome', with: 'Josimar Barbosa'
-    fill_in 'Data de nascimento', with: '01/01/2000'
+    fill_in 'Data de nascimento', with: '01/01/1998'
     click_on 'Salvar'
 
+    expect(user.profile).not_to be_nil
+    expect(user.profile).to eq(profile)
     expect(page).to have_content('Perfil atualizado com sucesso')
     expect(page).to have_content('meu@email.com')
     expect(page).to have_content('Josimar Barbosa')
-    expect(page).to have_content('01/01/2000')
+    expect(page).to have_content('01/01/1998')
   end
 end
